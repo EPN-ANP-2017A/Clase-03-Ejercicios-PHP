04261f696e8b33d1c43d9639b960d5d71b0ea637
Chalo Salvador <chalosalvador@gmail.com>
2017-05-02 19:00:37 -0500

    calse 3 ejercicios arreglos multd

--- actividades-en-clase/soluciones/ejercicio4/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Problema Propuesto:
- * Confeccionar un formulario que solicite la carga del nombre de usuario y su clave en dos oportunidades. En la página que se procesan los datos del formulario 
- * implementar una función que imprima un mensaje si las dos claves ingresadas son distintas.
- */
-?>
